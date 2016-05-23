c8fea7e27ade29977b3b46d82d29c94d1831a41b
nifex007 <nifex007@yahoo.com>
2016-05-23 11:38:12 +0100

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,20 +1,16 @@
 
-<<<<<<< HEAD
+
 var familyControllers = angular.module('familyControllers', []);
 
 familyControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
-=======
-var meApp = angular.module('meApp', []);
 
-meApp.controller('MyController', function($scope, $http){
->>>>>>> d507d316d04070a0b7629c0eda028290a3b10400
 		
 	$http.get('js/family_data.json').success(function(data) {
 
 		$scope.family = data;
 	});
 	
-<<<<<<< HEAD
+
 }]);
 
 familyControllers.controller('ViewController',['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
@@ -23,6 +19,4 @@ familyControllers.controller('ViewController',['$scope', '$http', '$routeParams'
 		$scope.memberId = $routeParams.memberId;
 	});
 }]);
-=======
-});
->>>>>>> d507d316d04070a0b7629c0eda028290a3b10400
+
