f26efa5ed2938089d9ee541a66670a7e40138e69
nifex007 <nifex007@yahoo.com>
2016-11-20 11:41:14 +0100

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -2,15 +2,10 @@
 
 var familyControllers = angular.module('familyControllers', []);
 
-familyControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
-
-		
+familyControllers.controller('ListController', ['$scope', '$http', function($scope, $http){		
 	$http.get('js/family_data.json').success(function(data) {
-
 		$scope.family = data;
 	});
-	
-
 }]);
 
 familyControllers.controller('ViewController',['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
